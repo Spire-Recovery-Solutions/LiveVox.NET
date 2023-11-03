fae344d39430cdfed62f4d371b20499c1b831e0f
Vimal Kumar <vimal.kumar@spirerecoverysolutions.com>
2023-11-03 17:47:50 +0000

    Implemented DeactivateCampaign

---
+++ LiveVox.NET/Models/Campaign/Requests/DeactivateCampaignRequest.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using LiveVox.NET.Models.Base;
+using System.Text.Json.Serialization;
+
+namespace LiveVox.NET.Models.Campaign.Requests
+{
+    public class DeactivateCampaignRequest : ILiveVoxRequest
+    {
+        public string? Category { get; set; } = "campaign";
+        public string? Resource { get; set; } = "campaigns/{id}/deactivate";
+        public Method RequestType { get; set; }
+        
+        public Task<RestRequest> BuildRequestAsync()
+        {
+            var request = new RestRequest(Resource.Replace("{id}", CampaignId.ToString()), RequestType);
+            // Serialize the request using the source-generated context for the specific type of 'request'
+            var requestBodyJson = JsonSerializer.Serialize(this, LiveVoxSerializerContext.Default.Options);
+
+            // Add the serialized request body to the RestRequest
+            request.AddJsonBody(requestBodyJson);
+            return Task.FromResult(request);
+        }
+        // <summary>
+        /// Campaign to be deactivated
+        /// </summary>
+        [JsonPropertyName("id")]
+        [Required]
+        public int CampaignId { get; set; }
+    }
+}
--- LiveVox.NET/Models/LiveVoxSerializerContext.cs
+++ LiveVox.NET/Models/LiveVoxSerializerContext.cs
@@ -3,18 +3,20 @@ using LiveVox.NET.Models.Campaign.AppendCampaign;
 using LiveVox.NET.Models.Campaign.Common;
 using LiveVox.NET.Models.Campaign.CreateCampaign;
 using LiveVox.NET.Models.Campaign.Enumerations;
+using LiveVox.NET.Models.Campaign.Requests;
 using LiveVox.NET.Models.Session;
 
 namespace LiveVox.NET.Models
 {
     [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, GenerationMode = JsonSourceGenerationMode.Default)]
     [JsonSerializable(typeof(AnsweringMachineOption))]
+    [JsonSerializable(typeof(CampaignType))]
     [JsonSerializable(typeof(SessionLoginRequest))]
     [JsonSerializable(typeof(SessionLoginResponse))]
     [JsonSerializable(typeof(AppendCampaignRequest))]
     [JsonSerializable(typeof(CreateCampaignRequest))]
     [JsonSerializable(typeof(CreateCampaignResponse))]
-    [JsonSerializable(typeof(CampaignType))]
+    [JsonSerializable(typeof(DeactivateCampaignRequest))]
     public partial class LiveVoxSerializerContext : JsonSerializerContext
     {
         // The source generator will fill in the necessary code here.
