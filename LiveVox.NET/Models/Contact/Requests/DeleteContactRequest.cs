674addf5aa3d59af4d63314c159304f38d9a2476
srs-binnys <binny.samuel@spirerecoverysolutions.com>
2023-11-03 16:55:36 +0000

    Updated Contact: added Delete request

---
+++ LiveVox.NET/Models/Contact/Requests/DeleteContactRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveVox.NET.Models.Base;
+using RestSharp;
+using static LiveVox.NET.LiveVoxRequestFactory;
+
+namespace LiveVox.NET.Models.Contact.Requests
+{
+    public class DeleteContactRequest : ILiveVoxRequest
+    {
+        public string? Category { get; set; } = "contact";
+        public string? Resource { get; set; } = "contacts";
+        public Method RequestType { get; set; } = Method.Delete;
+        public Task<RestRequest> BuildRequestAsync()
+        {
+            var request = new RestRequest(Category + "/" + Resource, RequestType);
+            request.AddQueryParameter("acct", Account);
+            return Task.FromResult(request);
+        }
+
+        public DeleteContactRequest(string account)
+        {
+            Account = account;
+        }
+
+        /// <summary>
+        /// The account of the contact you want to delete.
+        /// </summary>
+        public string Account { get; set; }
+    }
+}
