0bded85852152f8e68f9de91e64e597803571d92
Ayush Gupta <ayush.gupta@spirerecoverysolutions.com>
2023-11-13 14:36:30 -0600

    Implemented DeleteContextKey

--- LiveVox.NET/Models/Context/Requests/DeleteContextKeyRequest.cs
+++ LiveVox.NET/Models/Context/Requests/DeleteContextKeyRequest.cs
@@ -1,16 +1,15 @@
-﻿using LiveVox.NET.Models.Base;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
+using LiveVox.NET.Models.Base;
 using RestSharp;
 
 namespace LiveVox.NET.Models.Context.Requests
 {
-    public class DeleteContextDataRequest : ILiveVoxRequest
+    public class DeleteContextKeyRequest : ILiveVoxRequest
     {
-        [JsonIgnore] 
+        [JsonIgnore]
         public string? Category { get; set; } = "context";
-
-        [JsonIgnore] 
-        public string? Resource { get; set; } = "call/{session}/{txId}/{account}?client={client}";
+        [JsonIgnore]
+        public string? Resource { get; set; } = "call/{session}/{txId}/{account}/{key}?client={client}";
         [JsonIgnore]
         public Method RequestType { get; set; } = Method.Delete;
 
@@ -20,39 +19,45 @@ namespace LiveVox.NET.Models.Context.Requests
             request.AddParameter("session", SessionId, ParameterType.UrlSegment);
             request.AddParameter("txId", TransactionId, ParameterType.UrlSegment);
             request.AddParameter("account", Account, ParameterType.UrlSegment);
+            request.AddParameter("key", Key, ParameterType.UrlSegment);
             if (ClientId != null)
                 request.AddQueryParameter("client", ClientId.ToString());
-          
+
             return Task.FromResult(request);
         }
 
-        public DeleteContextDataRequest(string sessionId,int transactionId,string account,int clientId)
+        public DeleteContextKeyRequest(string sessionId, int transactionId, string account, string key, int clientId)
         {
             SessionId = sessionId;
             TransactionId = transactionId;
             Account = account;
+            Key = key;
             ClientId = clientId;
         }
 
         /// <summary>
-        /// The Session ID of the call to have its Context Data deleted
+        /// The Session ID of the call
         /// </summary>
         public string SessionId { get; set; }
 
         /// <summary>
-        /// The Transaction ID of the call to have its Context Data deleted
+        /// The Transaction ID of the call
         /// </summary>
         public int TransactionId { get; set; }
 
         /// <summary>
-        /// The Account of the call to have its Context Data deleted
+        /// The Account of the call
         /// </summary>
         public string Account { get; set; }
+
+        /// <summary>
+        /// The key to be deleted
+        /// </summary>
+        public string Key { get; set; }
 
         /// <summary>
         /// The ID of the Client the call is associated to
         /// </summary>
         public int? ClientId { get; set; }
-
     }
 }
