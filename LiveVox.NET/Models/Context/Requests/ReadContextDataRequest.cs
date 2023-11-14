9ca020b24b7b4ea5b5a9ce71460bb8ba2ee76306
Ayush Gupta <ayush.gupta@spirerecoverysolutions.com>
2023-11-14 08:55:30 -0600

    Implemented ReadContext Key and Data

---
+++ LiveVox.NET/Models/Context/Requests/ReadContextDataRequest.cs
@@ -0,0 +1,56 @@
+﻿using LiveVox.NET.Models.Base;
+using RestSharp;
+using System.Text.Json.Serialization;
+
+namespace LiveVox.NET.Models.Context.Requests
+{
+    public class ReadContextDataRequest :ILiveVoxRequest
+    {
+        [JsonIgnore]
+        public string? Category { get; set; } = "context";
+        [JsonIgnore]
+        public string? Resource { get; set; } = "call/{session}/{txId}/{account}?client={client}";
+        [JsonIgnore]
+        public Method RequestType { get; set; } = Method.Get;
+        public Task<RestRequest> BuildRequestAsync()
+        {
+            var request = new RestRequest(Category + "/" + Resource, RequestType);
+            request.AddParameter("session", SessionId, ParameterType.UrlSegment);
+            request.AddParameter("txId", TransactionId, ParameterType.UrlSegment);
+            request.AddParameter("account", Account, ParameterType.UrlSegment);
+            if (ClientId != null)
+                request.AddQueryParameter("client", ClientId.ToString());
+
+            return Task.FromResult(request);
+
+        }
+
+        public ReadContextDataRequest(string sessionId, int transactionId, string account, int clientId)
+        {
+            SessionId = sessionId;
+            TransactionId = transactionId;
+            Account = account;
+            ClientId = clientId;
+        }
+
+        /// <summary>
+        /// The Session ID of the call to be read
+        /// </summary>
+        public string SessionId { get; set; }
+
+        /// <summary>
+        /// The Transaction ID of the call to be read
+        /// </summary>
+        public int TransactionId { get; set; }
+
+        /// <summary>
+        /// The Account of the call to be read
+        /// </summary>
+        public string Account { get; set; }
+
+        /// <summary>
+        /// The ID of the Client the call is associated to
+        /// </summary>
+        public int? ClientId { get; set; }
+    }
+}
