785076b27e61e5bc8bd088d9192bfef8e00446a8
Vimal Kumar <vimal.kumar@spirerecoverysolutions.com>
2023-11-14 17:42:57 +0000

    Implemented Custom Exception class

--- LiveVox.NET/Exception/LiveVoxException.cs
+++ LiveVox.NET/Exception/LiveVoxException.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LiveVox.NET.Exception
 {
